90f8f390bf391cef6287747ecbd1e8cbb94d58a8
lingyunsheng <1841046698@qq.com>
2019-05-06 18:56:56 +0800

    小程序群聊

---
+++ wxapp/lol/vant-cloud (1)/miniprogram/pages/index/index.js
@@ -0,0 +1,106 @@
+// miniprogram/pages/index/index.js
+// 引入js
+import Notify from '../dist/notify/notify';
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    newGroupModal: false,
+    groupName: ""
+
+  },
+  showNewGroupModal() {
+    this.setData({
+      newGroupModal: true
+    })
+  },
+  closeDialog() {
+    this.setData({
+      newGroupModal: false
+    })
+  },
+  onGroupNameChange(event) {
+    // console.log(event)
+    this.setData({
+      groupName: event.detail
+    })
+  },
+  creatGroup() {
+    // 绑定this作用域，因为有回调函数
+    let self = this
+    if (self.data.groupName === '') {
+      Notify({
+        text: '起个名字吧',
+        duration: 1500,
+        selector: '#notify-selector',
+        backgroundColor: '#dc3545'
+      });
+      // self.setData({
+      //   newGroupModal: true
+      // })
+      // dialog自带方法
+      self.selectComponent('#new-group-modal').stopLoading()
+      return 
+    } else {
+      
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
