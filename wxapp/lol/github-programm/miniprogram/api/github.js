550800f0e129cfcd15c979e7ed781b4ca7386349
lingyunsheng <1841046698@qq.com>
2019-05-13 18:57:11 +0800

    github小程序

---
+++ wxapp/lol/github-programm/miniprogram/api/github.js
@@ -0,0 +1,19 @@
+
+const trendings = (data) => {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: 'https://github-trending-api.now.sh/repositories',
+      data,
+      success: (res) => {
+        resolve(res.data);
+      },
+      fail: (err) => {
+        reject(err);
+      }
+    })
+  })
+}
+
+module.exports = {
+  trendings,
+}
