2c2c2dbdf032e8e5cffa3d3ed7a3433838fab30f
lingyunsheng <1841046698@qq.com>
2019-05-23 21:16:42 +0800

    socket聊天发送

---
+++ socket/index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import morgan from 'morgan'
+import path from 'path'
+import socketIO from 'socket.io'
+const app = express()
+app.set('views', path.resolve(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(morgan('dev'))
+
+app.get('/', (request, response) => {
+    response.render('index')
+})
+let server = app.listen(3000, () => {
+    console.log('6666')
+})
+let io = socketIO(server)
+io.on('connection', (socket) => {
+    console.log('connected')
+    socket.on('disconnect',()=>{
+        console.log('disconnected')
+    })
+    socket.on('message',(msg) => {
+        io.emit('message', msg)
+      })
+    
+})
