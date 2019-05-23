616fa7b2b1e0e59aff8b8ebacf67790790e604d0
lingyunsheng <1841046698@qq.com>
2019-05-23 21:25:00 +0800

    node接口

--- socket/index.js
+++ socket/index.js
@@ -24,3 +24,11 @@ io.on('connection', (socket) => {
       })
     
 })
+
+var http = require('http')
+http.createServer(function(request,response){
+  response.writeHead(200,{'Content-Type':'text/plain'})
+  response.end('hello world')    
+}).listen(8080)
+
+console.log('66666')
