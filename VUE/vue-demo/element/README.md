c98a026eda9d9b3adcf474fb1c55e33e7d53eab0
lingyunsheng <1841046698@qq.com>
2019-05-29 18:59:56 +0800

    vue页面

--- VUE/vue-demo/element/README.md
+++ VUE/vue-demo/element/README.md
@@ -19,3 +19,18 @@ npm run build --report
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+vue init webpack element
+
+<el-input v-model="input" placeholder="请输入内容"></el-input>
+
+<script>
+export default {
+  data() {
+    return {
+      input: ''
+    }
+  }
+}
+</script>
+双向数据绑定
--- VUE/vue-demo/element/src/App.vue
+++ VUE/vue-demo/element/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+
     <router-view/>
+    <!-- 每个页面必须填路由 -->
   </div>
 </template>
 
@@ -12,12 +13,15 @@ export default {
 </script>
 
 <style>
+*{
+  margin:0;
+  padding: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
