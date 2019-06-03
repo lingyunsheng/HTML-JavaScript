953d01a89fc04f9f1560adfd34e39febf80a3545
lingyunsheng <1841046698@qq.com>
2019-06-03 15:14:13 +0800

    koa框架

---
+++ node/koa-template/index.js
@@ -0,0 +1,25 @@
+const Koa = require('koa');
+// 应用的实例
+// 监听端口,app用户看到的界面
+const app = new Koa();
+//  很多个
+const admin = new Koa();
+// 接受请求 req res 封装成 ctx。 body响应体   方法:use
+app.use(async (ctx, next) => {
+    console.log(1);
+    // 没有回应  koa   没有回应  自己处理了  not found
+    // 1交给下一个中间件
+    await next();
+    console.log(2);
+})
+// use中间件
+app.use(async (ctx) => {
+    console.log(3);
+    ctx.body = `hello Koa`;
+})
+app.listen(3000, () => {
+    console.log('server is running 3000');
+});
+admin.listen(4000, () => {
+    // console.log('server is running 4000');
+});
---
+++ node/koa-template/readme.md
@@ -0,0 +1,15 @@
+# koa
+node 框架
+tj : koa co command
+npm init -y 
+npm i koa -S
+
+## 中间件 middleWare
+流水线 请求经过很多中间件
+一个中间件处理完 可以交给下一个中间件
+koa 特色。
+是一个 async 方法。
+
+not fuound
+
+132
