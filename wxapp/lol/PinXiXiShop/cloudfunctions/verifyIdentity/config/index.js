c9e9efd3f53a9d01177ccacf7ff542134dfac7ac
lingyunsheng <1841046698@qq.com>
2019-06-07 22:59:58 +0800

    云开发登录注册

---
+++ wxapp/lol/PinXiXiShop/cloudfunctions/verifyIdentity/config/index.js
@@ -0,0 +1,10 @@
+// 配置文件
+const config = {
+    mpAppId: 'wxeae00915600c9e68', // 小程序appid
+    secret: '6e08adffe9efee80a1ac61e162d61c54', // 小程序的secret
+    envName: 'xxxlingyunsheng-0e0b69x', // TCB环境ID
+    time: 1000 * 60, // session有效时间1分钟 单位毫秒
+    sessionSecret: '6e08adffe9efee80a1ac61e162d61c54'
+  };
+  
+  module.exports = config;
--- wxapp/lol/PinXiXiShop/miniprogram/app.js
+++ wxapp/lol/PinXiXiShop/miniprogram/app.js
@@ -5,6 +5,10 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+    wx.cloud.init({
+     traceUser:true
+    })
+    
 
     // 登录
     wx.login({
